35df4f129918caf2ebb709e8a8dedfb10aa4b6b0
ILSHATGIZATULIN <gizatulin86@gmail.com>
2017-09-04 18:56:51 +0300

    подсключили альбом css

--- form/index.php
+++ form/index.php
@@ -86,7 +86,7 @@ function isCorrectLength($value ="name",$min,$max) {
 
 
     <!--<link rel="stylesheet" href="./css/bootstrap.min.css.">-->
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../../css/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 
